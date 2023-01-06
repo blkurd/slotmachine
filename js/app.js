0f848848a5a117fe77278e40e7d88d38e6b1ad27
blkurd <blkurd@hotmail.com>
2023-01-05 20:00:11 -0500

    Done cd ls '' '

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // An Array of emojis that will be used for the slot machine game
-const items = ["🍭", "😻", "💵"];
+const emojis = ["🍭", "😻", "💵"];
 
 // One way to define variables
 
@@ -22,20 +22,60 @@ let wins;
 
 // Cashed Elements which I am going to update repeatedly when the state changes.
 
-const boxEls = {
+const buttonOne = document.querySelector("#start");
+buttonOne.addEventListener("click", playSlot);
 
-  boxOne: document.querySelector("#box1"),
-  boxTwo: document.querySelector("#box2"),
-  boxThree: document.querySelector("#box3"),
+function playSlot(e) {
+
+  console.log('Here I am clicking')
+
+// In this function I want to randomly generate a result of emojis. I will write
+// a function below to help me do this. 
+
+
+
+
+
+  render();
+}
+
+
+// This function's task is to randomly return the emojis 
+// in the following array = ["🍭", "😻", "💵"]
+
+function getRandomEmojis(){
+
+
+
+
 
 };
 
 
+
+
+const buttonTwo = document.querySelector("#restart");
+buttonTwo.addEventListener("click", restart);
+
+function restart(e) {
+
+  console.log('Here I am clickin too')
+
+  render();
+}
+
+
+
+
+const boxEls = {
+  boxOne: document.querySelector("#box1"),
+  boxTwo: document.querySelector("#box2"),
+  boxThree: document.querySelector("#box3"),
+};
+
 const h2El = {
-   wins: document.querySelector("#win")
-  };
-
-
+  wins: document.querySelector("#win"),
+};
 
 // This is the function that will be called when a user reloads the page or restats the game.
 
@@ -60,64 +100,23 @@ function init() {
 init();
 
 // This function should be called in every controller function like init. and this function will make
-// the wininig result and the boxes empty. 
+// the wininig result and the boxes empty.
 
 function render() {
+  // This is one way to write it.
 
-// This is one way to write it.
+  // boxEls.boxOne.innerText = boxes.boxOne;
+  // boxEls.boxTwo.innerText = boxes.boxTwo;
+  // boxEls.boxThree.innerText = boxes.boxThree;
 
-// boxEls.boxOne.innerText = boxes.boxOne;
-// boxEls.boxTwo.innerText = boxes.boxTwo;
-// boxEls.boxThree.innerText = boxes.boxThree;
+  // Since that I am using the same naming of the boxEls varialbles and boxes variables,
+  // This is another way to make code DRY is to use the [for ... in loop] to loop over an object.
 
-// Since that I am using the same naming of the boxEls varialbles and boxes variables, 
-// This is another way to make code DRY is to use the [for ... in loop] to loop over an object. 
+  for (let key in boxes) {
+    boxEls[key].innerText = boxes[key];
+  }
 
-for (let key in boxes){
+  // This is th h2 element's written alone because it has a different naming.
 
-  boxEls[key].innerText = boxes[key];
-
-};
-
-// This is th h2 element's written alone because it has a different naming. 
-
-h2El.wins.innerText = wins.win;
-
-
- };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const play = document.getElementById("start");
-
-// const init = document.getElementById("restart");
-
-// play.addEventListener("click", (eo) => {
-//   play.style.backgroundColor = "red";
-
-//   for (let i = 0; i < items.length; i++) {
-//     play.boxAll.display(items[i]);
-//   }
-
-//   // play.items.forEach(boxAll => console.log(boxAll));
-
-//   // play.boxAll.innerText = items
-// });
-
-// init.addEventListener("click", (eo) => {
-//   init.style.backgroundColor = "lightblue";
-// });
+  h2El.wins.innerText = wins.win;
+}
